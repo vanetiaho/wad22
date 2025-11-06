c1aabe87c92dd5fde009e1ca422f19ce9c77f156
sztng <szeteng2601@gmail.com>
2025-11-06 19:18:11 +0800

    changed icon

--- src/components/crowd.vue
+++ src/components/crowd.vue
@@ -123,8 +123,6 @@ const submitCrowdLevel = async () => {
   }
 };
  
- 
-
 
 
 
@@ -161,14 +159,14 @@ const submitCrowdLevel = async () => {
             <div>
                 <h2>CROWD LEVEL</h2>
                 <div class="crowd-icons">
-                    <span
+                    <div
                         v-for="level in 3"
                         :key="level"
                         @click="setCrowdLevel(level)"
                         :class="['crowd-icon', { filled: level <= crowdLevel }]"
                     >
-                        ☕
-                    </span>
+                        <svg width="50" height="50" viewBox="0 0 47.5 47.5" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><defs><clipPath clipPathUnits="userSpaceOnUse" id="a"><path d="M0 38h38V0H0z"/></clipPath><clipPath clipPathUnits="userSpaceOnUse" id="b"><path d="M0 38h38V0H0z"/></clipPath></defs><g clip-path="url(#a)" transform="matrix(1.25 0 0 -1.25 0 47.5)"><path d="M0 0c0-5.522-8.059-10-18-10S-36-5.522-36 0s8.059 10 18 10S0 5.522 0 0" style="fill:#99aab5;fill-opacity:1;fill-rule:nonzero;stroke:none" transform="translate(37 11)"/><path d="M0 0c0-5.522-8.059-10-18-10S-36-5.522-36 0s8.059 10 18 10S0 5.522 0 0" style="fill:#ccd6dd;fill-opacity:1;fill-rule:nonzero;stroke:none" transform="translate(37 13)"/><path d="M0 0c-14.958 0-17 15-17 19h34c0-2-1.958-19-17-19" style="fill:#f5f8fa;fill-opacity:1;fill-rule:nonzero;stroke:none" transform="translate(19 6)"/><path d="M0 0c-1.357.938-3.103 1.694-5.121 2.25C-3.246 2.826-.57 2.559 0 0m2.503-2.692C4.945 7.43-7.278 5.014-9.701 3.106c-1.34.149-2.736.234-4.181.234-9.389 0-17-3.228-17-8.444s7.611-9.444 17-9.444 17 4.228 17 9.444c0 .862-.225 1.664-.615 2.412" style="fill:#ccd6dd;fill-opacity:1;fill-rule:nonzero;stroke:none" transform="translate(32.882 30.048)"/></g><g clip-path="url(#b)" transform="matrix(1.25 0 0 -1.25 0 47.5)"><path d="M0 0c0-3.866-6.716-7-15-7s-15 3.134-15 7 6.716 7 15 7S0 3.866 0 0" style="fill:#8a4b38;fill-opacity:1;fill-rule:nonzero;stroke:none" transform="translate(34 24)"/><path d="M0 0a1 1 0 0 0-.707.293C-3.044 2.63-3.083 5.178-.832 8.555c.739 1.109.9 2.245.478 3.377-.461 1.235-1.438 1.996-1.731 2.076-.553 0-.958.444-.958.996 0 .553.491.996 1.043.996.997 0 2.395-1.153 3.183-2.625 1.034-1.933.91-4.039-.351-5.93C-1.129 4.503-.699 3.113.707 1.707A.999.999 0 0 0 0 0" style="fill:#d99e82;fill-opacity:1;fill-rule:nonzero;stroke:none" transform="translate(21 20)"/><path d="M0 0a1 1 0 0 0-.707.293C-3.044 2.63-3.083 5.178-.832 8.555c.727 1.091.894 2.082.494 2.947-.444.961-1.431 1.469-1.684 1.499a.99.99 0 0 0-.989.999c0 .553.459 1 1.011 1 .997 0 2.584-.974 3.36-2.423.481-.899 1.053-2.761-.528-5.132C-1.129 4.503-.699 3.113.707 1.707A.999.999 0 0 0 0 0" style="fill:#d99e82;fill-opacity:1;fill-rule:nonzero;stroke:none" transform="translate(15 22)"/></g></svg>
+                    </div>
                 </div>
             </div>
             <div>
@@ -296,12 +294,19 @@ const submitCrowdLevel = async () => {
 }
 
 .crowd-icon {
-    font-size: 50px;
     cursor: pointer;
     transition: all 0.2s ease;
     opacity: 0.3;
     filter: grayscale(100%);
     user-select: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.crowd-icon svg {
+    width: 50px;
+    height: 50px;
 }
 
 .crowd-icon:hover {
